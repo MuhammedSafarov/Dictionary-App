403703fbe768f6ad2329e8d55d28ce6fd3b7f62c
Muxammed Safarov <muxammed@Muxammeds-MacBook-Pro.local>
2023-07-18 14:56:52 +0400

    Optimized ui

--- ms-dictionary/src/home/Content.jsx
+++ ms-dictionary/src/home/Content.jsx
@@ -1,27 +1,56 @@
-import React from "react";
-import './Content.scss';
-import {FaPlay} from 'react-icons/fa';
+import React, { useState } from "react";
+import "./Content.scss";
+import { FaPlay, FaSearch } from "react-icons/fa";
+import axios from "axios";
 
 const Content = () => {
+  let [keyWord, setKeyWord] = useState([]);
+  let [searchedKeyWord, setSearchedKeyWord] = useState("");
+
+  const findWord = async (e) => {
+    e.preventDefault();
+    let res = await axios.get(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`
+    );
+    let data = res?.data[0];
+    setSearchedKeyWord(data);
+  };
+
+  console.log(searchedKeyWord);
   return (
     <div className="home-content">
-      <form>
-        <input type="text" className="search" />
+      <form onSubmit={findWord}>
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search ..."
+          onChange={(e) => setKeyWord(e.target.value.toString())}
+        />
+        <button className="search-btn">
+          <FaSearch className="search-icon" />
+        </button>
       </form>
       <div className="first-part">
         <div>
-          <h3>Searched Input</h3>
-          <p>[ ko; ]</p>
+          <h3>{searchedKeyWord?.word}</h3>
+          <p>{searchedKeyWord?.phonetic}</p>
         </div>
-        <button className='play-btn'><FaPlay/></button>
+        <button className="play-btn">
+          <FaPlay />
+        </button>
       </div>
       <div className="second-part">
-          <b>Noun</b>
-          <h3>Meaning</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, quisquam.</p>
+        {/* <b>{searchedKeyWord?.meanings[0]?.definitions[0]}</b> */}
+        <h3>Meaning</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+          quisquam.
+        </p>
       </div>
-      <div className='synonims'>
-          <p><b>Synonims:</b> Lorem ipsum dolor sit amet.</p>
+      <div className="synonims">
+        <p>
+          <b>Synonims:</b> Lorem ipsum dolor sit amet.
+        </p>
       </div>
     </div>
   );
